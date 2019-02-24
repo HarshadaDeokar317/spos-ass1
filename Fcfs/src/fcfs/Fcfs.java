45ed933f8ae1d67f7a7af01825db6a6acaf60d1e
HarshadaDeokar317 <harshadatd1998@gmail.com>
2019-02-24 14:17:13 +0530

    i added line

--- Fcfs/src/fcfs/Fcfs.java
+++ Fcfs/src/fcfs/Fcfs.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Fcfs {
 
-    
+    //i added line
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter no of proceses=");
